d30621b68a00bdd645f077e951b1a26936b03eb7
Babajide Olowoyo <olowoyobabajide@gmail.com>
2025-04-21 18:35:50 +0100

    nftw checks the file for (.apk). Moving to checking for patterns with glob

--- code/ch_dir.c
+++ code/ch_dir.c
@@ -8,67 +8,43 @@
 #include <ftw.h>
 
 int extension(const char *a);
-char scan_dir(char *u);
 static int nfile(const char *path, const struct stat *sb, int typeflag, struct FTW *ftbuf);
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     char buffer[256];
-    if (argc != 2)
+    if (argc < 2)
     {
         printf("Too much input\n");
     }
-    scanf(argv[1], "%255s", &buffer);
+    sscanf(argv[1], "%255s", &buffer);
 
     //scan_dir(argv[1]);
-    nftw(argv[1],nfile, 5, FTW_PHYS);
+    
+    if (nftw(argv[1],nfile, 10, FTW_PHYS) == -1)
+    {
+        perror("nftw");
+        return 1;
+    }
 }
 
 static int nfile(const char *path, const struct stat *sb, int typeflag, struct FTW *ftbuf)
 {
-    if (typeflag == FTW_F)
+    /*if (typeflag == FTW_D)
     {
-       
+        printf("This is a directory\n");
+        printf("Directory: %s\n", path);
+        mode_t perms = sb->st_mode & 0777;
+        printf("Permission: %03o\n", perms);
+
+    }*/
+    if (typeflag == FTW_F && extension(path))
+    {
         printf("This is a file\n");
-        if (extension(path))
-        {
-            printf("Files\n");
-        }
-
+        printf("FILE: %s\n", path);
     }
-
+    return 0;
 }
-/**
-char scan_dir(char *u)
-{
-
-    struct dirent *de;
-    struct stat buf;
-
-    DIR *jide = opendir(u);
-
-    if (jide == NULL)
-    {
-        printf("Nothing here!\n");
-    }
-    stat(u, &buf);
-    mode_t perms = buf.st_mode & 0777;
-    printf("Permission: %03o\n", perms);
-
-    while ((de = readdir(jide)) != NULL )
-    {
-        
-        if(de->d_type == DT_REG) 
-        {    
-            extension(de->d_name);
-        }
-    }
-    closedir(jide);
-}
-    */
-/**
-This function checks if a particular file is an apk ???
-*/
 int extension(const char *a)
 {
     const char *dot = strchr(a, '.');
@@ -80,7 +56,7 @@ int extension(const char *a)
     const char *ext = dot + 1;
     if(strlen(ext) == 3 && tolower(ext[0]) == 'a' && tolower(ext[1]) == 'p' && tolower(ext[2]) == 'k')
     {
-        printf("%s", a);
+        printf("%s\n", a);
     }  
     else
     {
