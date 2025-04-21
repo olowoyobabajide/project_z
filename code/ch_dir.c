3ffb837824507b28d6ed2bb58fef23df41144b7c
Babajide Olowoyo <olowoyobabajide@gmail.com>
2025-04-21 17:26:11 +0100

    working on using nftw for the traversing the files

--- code/ch_dir.c
+++ code/ch_dir.c
@@ -1,40 +1,44 @@
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <ctype.h>
+#include <ftw.h>
 
-int extension(char *a);
+int extension(const char *a);
 char scan_dir(char *u);
+static int nfile(const char *path, const struct stat *sb, int typeflag, struct FTW *ftbuf);
 
 void main(int argc, char **argv)
 {
     char buffer[256];
-    if (argc != 1)
+    if (argc != 2)
     {
         printf("Too much input\n");
     }
     scanf(argv[1], "%255s", &buffer);
-    nftw("/home", )
 
-    return scan_dir(argv[1]);
+    //scan_dir(argv[1]);
+    nftw(argv[1],nfile, 5, FTW_PHYS);
 }
 
 static int nfile(const char *path, const struct stat *sb, int typeflag, struct FTW *ftbuf)
 {
-    path = "/home";
-    if (typeflag & FTW_D)
+    if (typeflag == FTW_F)
     {
-        printf("This is a directory");
-        if ((sb.st_mode & 0777) == 755)
+       
+        printf("This is a file\n");
+        if (extension(path))
         {
-            printf("Android diretory");
+            printf("Files\n");
         }
 
     }
 
 }
+/**
 char scan_dir(char *u)
 {
 
@@ -61,10 +65,11 @@ char scan_dir(char *u)
     }
     closedir(jide);
 }
+    */
 /**
 This function checks if a particular file is an apk ???
 */
-int extension(char *a)
+int extension(const char *a)
 {
     const char *dot = strchr(a, '.');
     if (!dot || dot == a)
