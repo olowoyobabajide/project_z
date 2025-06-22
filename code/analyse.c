cb47c1950eb00d14c86fb978bcc0746c4fcc8ad8
Babajide Olowoyo <olowoyobabajide@gmail.com>
2025-06-23 00:30:51 +0100

    keep going

--- code/analyse.c
+++ code/analyse.c
@@ -5,12 +5,14 @@ extern char buffer[PATH_MAX];
 Over here we analyse the android manifest file for risky permissions and flags
 */
 
-/*enum permission {SEND_SMS = 1, RECEIVE_SMS, RECEIVE_SMS, CALL_PHONE, READ_CALL_LOG, WRITE_CALL_LOG, RECORD_AUDIO, CAMERA,
-READ CONTACTS, WRITE_CONTACTS, ACCESS_FINE_LOCATION, INTERNET, SYSTEM_ALERT_WINDOW, INSTALL_PACKAGES,
-READ_EXTERNAL_STORAGE};*/
+const char *permission[] = {"android.permission.SEND_SMS", "android.permission.READ_SMS", "android.permission.RECEIVE_SMS ", "android.permission.CALL_PHONE", "android.permission.READ_CALL_LOG", "android.permission.WRITE_CALL_LOG", "android.permission.RECORD_AUDIO", "android.permission.CAMERA",
+"android.permission.READ_CONTACTS", "android.permission.WRITE_CONTACTS", "android.permission.ACCESS_FINE_LOCATION","android.permission.ACCESS_COARSE_LOCATION", "android.permission.INTERNET", "android.permission.SYSTEM_ALERT_WINDOW ", "android.permission.REQUEST_INSTALL_PACKAGES",
+"android.permission.INSTALL_PACKAGES", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
 
 typedef struct{
     char low[PATH_MAX];
+    char medium[PATH_MAX];
+    char high[PATH_MAX];
 }pers;
 
 int jide(char *a)
@@ -19,7 +21,7 @@ int jide(char *a)
     
     pers mypers;
     //snprintf(temp, PATH_MAX, "grep 'jide' %s/temp/AndroidManifest.xml", a);
-    
+    FILE *test = fopen("low_perm.txt", "rw+");
     FILE *jide;
     if ((jide = fopen("AndroidManifest.xml", "rb")) == NULL)
     {
@@ -27,19 +29,20 @@ int jide(char *a)
     }
     //mypers.low = malloc(PATH_MAX);
     printf("[Manifest Scan...]\n");
-    while(fgets(temp, PATH_MAX-1, jide) != NULL)
+    char *s;
+    char *string = "android";
+    while(fgets(temp, PATH_MAX-1, jide))
     {
-        char *s;
-        if((s = strstr(temp, "RECORD_AUDIO")) != NULL || (s = strstr(temp, "VIBRATE")) != NULL)
+        if((s = strstr(temp, "<uses-permission>"))) 
         {
-            for(int i = 0; i < PATH_MAX; i++)
+            while(!feof(test))
             {
-                char *tok = strtok(s, "\"/>");
-                snprintf(mypers.low, PATH_MAX-1, "%s", tok[i]);
+                fprintf(test, "%s\n", s);
             }
         }
     }
     printf("These are low level permission threats\n");
-    printf("Permissions: %s", mypers.low);
     fclose(jide);
+    fclose(test);
+
 }
