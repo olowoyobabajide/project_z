be7b20d0724de24d2e92d791f6bc501628615dc2
Babajide Olowoyo <olowoyobabajide@gmail.com>
2025-07-23 02:06:35 +0100

    ok

--- code/analyse.c
+++ code/analyse.c
@@ -1,12 +1,12 @@
 #include "main.h"
 //extern char buffer[PATH_MAX];
-/**
+/*
 Over here we analyse the android manifest file for risky permissions and flags
 */
 
-/*const char *permission[] = {"android.permission.SEND_SMS", "android.permission.READ_SMS", "android.permission.RECEIVE_SMS ", "android.permission.CALL_PHONE", "android.permission.READ_CALL_LOG", "android.permission.WRITE_CALL_LOG", "android.permission.RECORD_AUDIO", "android.permission.CAMERA",
+const char *permission[18] = {"android.permission.SEND_SMS", "android.permission.READ_SMS", "android.permission.RECEIVE_SMS ", "android.permission.CALL_PHONE", "android.permission.READ_CALL_LOG", "android.permission.WRITE_CALL_LOG", "android.permission.RECORD_AUDIO", "android.permission.CAMERA",
 "android.permission.READ_CONTACTS", "android.permission.WRITE_CONTACTS", "android.permission.ACCESS_FINE_LOCATION","android.permission.ACCESS_COARSE_LOCATION", "android.permission.INTERNET", "android.permission.SYSTEM_ALERT_WINDOW ", "android.permission.REQUEST_INSTALL_PACKAGES",
-"android.permission.INSTALL_PACKAGES", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};*/
+"android.permission.INSTALL_PACKAGES", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
 
 
 int analyse_per(char *a)
@@ -59,8 +59,13 @@ int analyse_per(char *a)
 }
 
 
-void tag_check(char *a){
+void tag_check(char *a)
+{
+    const char *permission[18] = {"android.permission.SEND_SMS", "android.permission.READ_SMS", "android.permission.RECEIVE_SMS ", "android.permission.CALL_PHONE", "android.permission.READ_CALL_LOG", "android.permission.WRITE_CALL_LOG", "android.permission.RECORD_AUDIO", "android.permission.CAMERA",
+"android.permission.READ_CONTACTS", "android.permission.WRITE_CONTACTS", "android.permission.ACCESS_FINE_LOCATION","android.permission.ACCESS_COARSE_LOCATION", "android.permission.INTERNET", "android.permission.SYSTEM_ALERT_WINDOW ", "android.permission.REQUEST_INSTALL_PACKAGES",
+"android.permission.INSTALL_PACKAGES", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
     FILE *file = fopen(a, "rb");
+    FILE *log = fopen("log.txt", "w");
     char perm[PATH_MAX];
 
     if (file == NULL)
@@ -72,18 +77,33 @@ void tag_check(char *a){
     {
         while(fgets(perm, PATH_MAX-1, file))
         {
+            if (strstr(perm, "android:name=\"") == NULL)
+            {
+                perror("\"android.name\" could not be found");
+            }
+            else
+            {
                 ptr = strstr(perm, "android:name=\"");
                 ptr += 14;
                 char *end;
                 if (end = strchr(ptr, '"'))
                 {
-                    end = strchr(ptr, '"');
-                     end = '\0';
-                     printf("%s", ptr);
+                    *end = '\0';
+                    printf("%s\n", ptr);
+                    for (int i = 0; i < 13; i++)
+                    {
+                        if (strcmp(permission[i], ptr)== 0)
+                        {
+                            char *temp;
+                            snprintf(temp, PATH_MAX-1, "Permission: %s\nRisk Level: DANGEROUS\nReason: Accesses user's info\n ------", permission[i]);
+                            
+                            printf("yes\n");
+                        }
+                    }
                 }
-                //printf("%s", ptr);
-                ptr -= 14;
+            }
         }
     }
     fclose(file);
+
 }
