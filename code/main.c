a9af5e3a43d5927a6e29f53168b5050b87f41409
olowoyobabajide <olowoyobabajide@gmail.com>
2026-01-22 03:24:54 +0100

    Add `--help` and `--version` command-line options to fs_analyzer.

--- code/main.c
+++ code/main.c
@@ -16,7 +16,19 @@ int main(int argc, char **argv)
     }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-o") == 0) {
+        if(strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0){
+            printf("Usage: ./fs_analyzer <path> [-d] [-o json <report_file.json>]\n\n");
+            printf("Options:\n");
+            printf("\t-d: Enable dex log\n");
+            printf("\t-o json <report_file.json>: Output report in JSON format\n");
+            printf("\t--help or -h: Show this help message\n");
+            printf("\t--version or -v: Show version\n");
+            return EXIT_SUCCESS;
+        }
+        else if(strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0){
+            printf("Version: fs_analyzer 1.0.0\n");
+            return EXIT_SUCCESS;
+        } else if (strcmp(argv[i], "-o") == 0) {
             if (i + 2 < argc) {
                 if (strcmp(argv[i+1], "json") == 0) {
                     json_output_file = argv[i+2];
@@ -33,18 +45,8 @@ int main(int argc, char **argv)
             enable_dex_log = 1;
         } else if (input_path == NULL) {
             input_path = argv[i];
-        } else if(strcmp(argv[1], "--help") == 0 || strcmp(argv[i], "-h") == 0){
-            printf("Usage: ./fs_analyzer <path> [-d] [-o json <report_file.json>]\n\n");
-            printf("Options:\n");
-            printf("\t-d: Enable dex log\n");
-            printf("\t-o json <report_file.json>: Output report in JSON format\n");
-            printf("\t--help or -h: Show this help message\n");
-            printf("\t--version or -v: Show version\n");
-            return EXIT_SUCCESS;
-        }
-        else if(strcmp(argv[1], "--version") == 0 || strcmp(argv[i], "-v") == 0){
-            printf("Version: fs_analyzer 1.0.0\n");
-            return EXIT_SUCCESS;
+        } else {
+             // Optional: Handle unknown flags or extra arguments
         }
     }
 
